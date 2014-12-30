6b065a6d9a01776627f73c2631e3859c6b97d5fd
joelmwas <joelmwas199@gmail.com>
2014-12-30 09:12:46 +0300

    data visualization using web console

--- index.php
+++ index.php
@@ -1,4 +1,35 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script type="text/javascript">
+	AmCharts.loadJSON = function(url) {
+  // create the request
+  if (window.XMLHttpRequest) {
+    // IE7+, Firefox, Chrome, Opera, Safari
+    var request = new XMLHttpRequest();
+  } else {
+    // code for IE6, IE5
+    var request = new ActiveXObject('Microsoft.XMLHTTP');
+  }
+
+  // load it
+  // the last "false" parameter ensures that our code will wait before the
+  // data is loaded
+  request.open('GET', url, false);
+  request.send();
+
+  // parse adn return the output
+  return eval(request.responseText);
+};
+
+</script>
+</head>
+<body>
+
+
 <?php   
+
 
 // Connect to MySQL
 $link = mysql_connect( 'localhost', 'root', 'wewe' );
@@ -47,17 +78,28 @@ echo "\n]";
 
 
 
+
+
 // Close the connection
 mysql_close($link);
 
-
-
-
-
-
-
-
-
 ?>
 
+<script type="text/javascript">
+	AmCharts.ready(function() {
+   // load the data
+   var chartData = AmCharts.loadJSON('data.php');
 
+   // this is a temporary line to verify if the data is loaded and parsed correctly
+   // please note, that console.debug will work on Safari/Chrome only
+   console.debug(chartData);
+
+   // build the chart
+   // ...
+ });
+</script>
+
+
+
+</body>
+</html>
