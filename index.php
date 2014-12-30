b65b0d32a9afd02d3a42fa3cdbc879293be44910
joelmwas <joelmwas199@gmail.com>
2014-12-30 08:53:18 +0300

    data in rows

--- index.php
+++ index.php
@@ -1,6 +1,39 @@
 <?php   
 
+// Connect to MySQL
+$link = mysql_connect( 'localhost', 'root', 'wewe' );
+if ( !$link ) {
+  die( 'Could not connect: ' . mysql_error() );
+}
 
+// Select the data base
+$db = mysql_select_db( 'datavis', $link );
+if ( !$db ) {
+  die ( 'Error selecting database \'datavis\' : ' . mysql_error() );
+}
+
+// Fetch the data
+$query = "
+  SELECT *
+  FROM my_chart_data
+  ORDER BY category ASC";
+$result = mysql_query( $query );
+
+// All good?
+if ( !$result ) {
+  // Nope
+  $message  = 'Invalid query: ' . mysql_error() . "\n";
+  $message .= 'Whole query: ' . $query;
+  die( $message );
+}
+
+// Print out rows
+while ( $row = mysql_fetch_assoc( $result ) ) {
+  echo $row['category'] . ' | ' . $row['value1'] . ' | ' .$row['value2'] . "\n";
+}
+
+// Close the connection
+mysql_close($link);
 
 
 
